f5ae93d63ab9968e64e0624d8630c7baa520ce5c
Steve Vance <steve.vance@hotmail.co.uk>
2021-04-06 14:21:37 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="react-logo" />
         <h1>We now have Auth!</h1>
       </header>
       <AmplifySignOut />
